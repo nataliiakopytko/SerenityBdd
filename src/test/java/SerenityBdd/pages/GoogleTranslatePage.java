b238fd36e6d8adc5c21ea40ce10864483c1d4f99
nataliiakopytko <natalkapoltavka950@gmail.com>
2019-02-18 21:52:24 +0200

    Changing xpaths on google translate page

--- src/test/java/SerenityBdd/pages/GoogleTranslatePage.java
+++ src/test/java/SerenityBdd/pages/GoogleTranslatePage.java
@@ -16,13 +16,13 @@ public class GoogleTranslatePage extends PageObject {
     @FindBy(xpath = "//div[@class = 'sl-more tlid-open-source-language-list'] ")
     private WebElement moreButtonLeft;
 
-    @FindBy(xpath = "//div[@aria-label and contains(text(), 'англійська')]")
+    @FindBy(xpath = "//div[@class = 'language_list_section']//div[contains(@onclick, 'sl_list_en')]")
     private WebElement firstLanguage;
 
     @FindBy(xpath = "//div[@class = 'tl-more tlid-open-target-language-list']")
     private WebElement moreButtonRight;
 
-    @FindBy(xpath = "//div[@aria-label and contains(text(), 'українська')]")
+    @FindBy(xpath = "//div[@class = 'language_list_section']//div[@class = 'language_list_item_icon tl_list_uk_checkmark']")
     private WebElement secondLanguage;
 
     public String getFirstLanguage(){
